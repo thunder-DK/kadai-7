f1586817834ab70c456b629821c299b2d41d9336
くろだだいすけ <d_k_96@yahoo.co.jp>
2015-09-17 23:44:39 +0900

    まだブラッシュアップ中ですが、一旦提出致します。 まだまだこれからブラッシュアップを続けるので、更新でき次第コミット致します。

---
+++ admin/index.php
@@ -0,0 +1,13 @@
+<html>
+<head>
+<meta charset="utf-8">
+<title></title>
+</head>
+<body>
+<ul>
+<li><a href="input.php">ニュース新規追加</a></li>
+<li><a href="news_list.php">ニュース一覧（更新はここから）</a></li>
+<li><a href="search_ps.php">ニュース検索</a></li>
+</ul>
+</body>
+</html>
